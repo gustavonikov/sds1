81b082e759e86d02d3082494c55b41f182ff4147
gustavonikov <gustavonikov@gmail.com>
2020-09-18 01:00:03 -0300

    Adicionando frontend-web

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": [
+        "plugin:react/recommended",
+        "airbnb"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        'linebreak-style': ['off'],
+        'object-curly-newline': ['off'],
+        'react/jsx-indent': ['off'],
+        'react/jsx-indent-props': ['off'],
+        indent: ['warn', 4],
+        'no-else-return': ['error', { allowElseIf: true }],
+        'react/jsx-one-expression-per-line': ['off'],
+        'react/no-array-index-key': ['off'],
+        'no-console': ['off'],
+        'no-return-assign': ['off'],
+        'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+        'import/extensions': ['error', 'never', {
+            tsx: 'never',
+            svg: 'never',
+        }],
+        "react/prop-types": ['off'],
+        'max-len': ['warn', { 'code': 150}],
+    }
+};
